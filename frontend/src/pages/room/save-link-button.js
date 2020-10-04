768c59cb7c3b21b25ca5d203b9135043a95b7d4a
Evgesha Zubik <ev.zubik@yandex.by>
2020-10-04 20:08:31 +0300

    implement copy link functionality

--- frontend/src/pages/room/save-link-button.js
+++ frontend/src/pages/room/save-link-button.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { IconButton, Tooltip, Zoom } from '@material-ui/core';
 import SaveIcon from '@material-ui/icons/Save';
 
 const SaveLinkButton = () => {
+  const handleButtonClick = useCallback(() => {
+    let dummy = document.createElement('input');
+    document.body.appendChild(dummy);
+    dummy.value = window.location.href;
+    dummy.select();
+    document.execCommand('copy');
+    document.body.removeChild(dummy);
+  }, []);
+
   return (
     <Tooltip TransitionComponent={Zoom} title="Click for saving room link">
-      <IconButton
-        onClick={() => {
-          var url = window.location.href;
-        }}
-      >
+      <IconButton onClick={handleButtonClick}>
         <SaveIcon fontSize="large" />
       </IconButton>
     </Tooltip>
