a1bf5db2533c0cf551f8d00c06d5b04d1ac18e89
zhenekZK <ev.zubik@yandex.by>
2020-10-06 12:08:27 +0300

    fix typing block position

--- frontend/src/pages/room/typing-info.js
+++ frontend/src/pages/room/typing-info.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Typography } from '@material-ui/core';
+import { Box, Typography } from '@material-ui/core';
 
 const TypingInfo = ({ typingUsers }) => {
   if (!typingUsers.length) {
@@ -8,13 +8,15 @@ const TypingInfo = ({ typingUsers }) => {
   }
 
   return (
-    <Typography>
-      {typingUsers.length === 1
-        ? `${typingUsers[0].username} is typing...`
-        : `${typingUsers
-            .map((user) => user.username)
-            .join(', ')} are typing...`}
-    </Typography>
+    <Box position="absolute" top={-30}>
+      <Typography>
+        {typingUsers.length === 1
+          ? `${typingUsers[0].username} is typing...`
+          : `${typingUsers
+              .map((user) => user.username)
+              .join(', ')} are typing...`}
+      </Typography>
+    </Box>
   );
 };
 
